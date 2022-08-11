e0aa9b9e88a5411db4cabf001c16d5bd675656da
vathanac <sovanvathanaknorng@gmail.com>
2022-08-12 00:02:01 +0700

    add folders to .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:app_31/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ButtonApp());
+  runApp(const ButtonApp());
 }
 
 class ButtonApp extends StatelessWidget {
+  const ButtonApp({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: HomeScreen(),
